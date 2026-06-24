9e3f02d66dd6e1d0b38147c80f9f1f1f96fb3fa9
kommuMeriteja <meriteja5003@gmail.com>
2026-06-24 15:37:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Navbar color change on scroll
+
+window.addEventListener("scroll", function () {
+
+    let navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
